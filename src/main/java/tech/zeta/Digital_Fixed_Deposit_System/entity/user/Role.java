e373f8029cdf4aaeba08f272947083d4dcc73160
Priyanshu Mishra <172821911+priyanshu936932@users.noreply.github.com>
2026-02-07 20:51:55 +0530

    Revert "Authentication added"

--- src/main/java/tech/zeta/Digital_Fixed_Deposit_System/entity/user/Role.java
+++
@@ -1,6 +0,0 @@
-package tech.zeta.Digital_Fixed_Deposit_System.entity.user;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- src/main/java/tech/zeta/Digital_Fixed_Deposit_System/exception/GlobalExceptionHandler.java
+++ src/main/java/tech/zeta/Digital_Fixed_Deposit_System/exception/GlobalExceptionHandler.java
@@ -1,59 +1,7 @@
 package tech.zeta.Digital_Fixed_Deposit_System.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import tech.zeta.Digital_Fixed_Deposit_System.dto.common.ApiResponse;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ApiResponse> handleBusinessException(
-            BusinessException ex
-    ) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ApiResponse(
-                        ex.getMessage(),
-                        HttpStatus.BAD_REQUEST.value()
-                ));
-    }
-
-    @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<ApiResponse> handleUnauthorizedException(
-            UnauthorizedException ex
-    ) {
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(new ApiResponse(
-                        ex.getMessage(),
-                        HttpStatus.UNAUTHORIZED.value()
-                ));
-    }
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse> handleResourceNotFoundException(
-            ResourceNotFoundException ex
-    ) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(new ApiResponse(
-                        ex.getMessage(),
-                        HttpStatus.NOT_FOUND.value()
-                ));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse> handleGenericException(
-            Exception ex
-    ) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ApiResponse(
-                        "Internal server error",
-                        HttpStatus.INTERNAL_SERVER_ERROR.value()
-                ));
+public class GlobalExceptionHandler extends RuntimeException{
+    public GlobalExceptionHandler(String message){
+        super(message);
     }
 }
